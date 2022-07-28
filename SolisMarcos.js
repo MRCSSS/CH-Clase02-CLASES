69415e689db0ce9ec6317fedb64b014803ed302e
Marcos Solis S <mss@192.168.1.16>
2022-07-27 20:16:01 -0500

    Last update

--- SolisMarcos.js
+++ SolisMarcos.js
@@ -7,27 +7,24 @@ class Usuario { // Creamos la Clase Usuario
     }
 
     getFullName() {         // Método de obtención del nombre completo del usuario
-        console.log(`Nombre completo: ${this.nombre} ${this.apellido}`);
+        return (`${this.nombre} ${this.apellido}`)
     }
 
     addMascota(pet) {       // Método para agregar una mascota a la lista de Mascotas
         this.mascotas.push(pet);
-        console.log(`${pet} agregado a Mascotas de ${this.nombre} ${this.apellido}`);
     }
 
     countMascotas() {       // Método para contar la cantidad de mascotas en la lista de Mascotas
-        let cantidadMascotas = this.mascotas.length;
-        console.log(`Cantidad de mascotas de ${this.nombre} ${this.apellido}: ${cantidadMascotas}`)
+        return this.mascotas.length;
     }
 
     addBook(author, book) { // Método para agregar un libro con nombre y autor a la lista de Libos
         this.libros.push({nombre: book, autor: author});
-        console.log(`Libro '${book}' de ${author} agregado a Libros de ${this.nombre} ${this.apellido}`);
     }
 
     getBookNames() {        // Método para obtener los nombres de los libros de la lista de Libros
         let bookNames = this.libros.map( (libro) => { return libro.nombre; })
-        console.log(`Libros de ${this.nombre} ${this.apellido}: `,bookNames);
+        return bookNames;
     }
 }
 
@@ -35,20 +32,27 @@ class Usuario { // Creamos la Clase Usuario
 const user1 = new Usuario('Marcos', 'Solis');
 
 // Obtenemos nombre completo del Usuario user1
-user1.getFullName();
+console.log('>> Obtenemos nombre completo del Usuario user1')
+console.log('   Nombre completo de usuario: ', user1.getFullName());
 
 // Revisamos la cantidad de mascotas en la lista Mascotas del user1
-user1.countMascotas();
+console.log('>> Revisamos la cantidad de mascotas en la lista Mascotas del user1')
+console.log('   Cantidad de mascotas de ', user1.getFullName(), '-> ', user1.countMascotas());
 // Agregamos mascotas a la lista Mascotas del Usuario user1
+console.log('>> Agregamos mascotas a la lista Mascotas del Usuario user1')
 user1.addMascota('Perro');
 user1.addMascota('Gato');
 user1.addMascota('Hamster');
-// Volvemos a revisamos la cantidad de mascotas en la lista Mascotas del user1
-user1.countMascotas();
+// // Volvemos a revisamos la cantidad de mascotas en la lista Mascotas del user1
+console.log('>> Volvemos a revisamos la cantidad de mascotas en la lista Mascotas del user1')
+console.log('   Cantidad de mascotas de ', user1.getFullName(), '-> ', user1.countMascotas());
 
 // Agregamos libros con autor a la lista Libros del user1
+console.log('>> Agregamos libros con autor a la lista Libros del user1')
 user1.addBook('George Orwell', '1984');
 user1.addBook('Carmen Laforet', 'Nada');
 user1.addBook('Víctor Hugo', 'Los Miserables');
 // Obtenemos únicamente los nombres de los libros de la lista Libros del Usuario user1
-user1.getBookNames();
+console.log('>> Obtenemos únicamente los nombres de los libros de la lista Libros del Usuario user1')
+console.log('   Libros de ', user1.getFullName(), '->', user1.getBookNames());
+
